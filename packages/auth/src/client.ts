70d1009061f882f19a46d8e25035e906789f1051
tzhang96 <tzhang96@g.ucla.edu>
2025-01-31 00:10:40 -0500

    Fixed Vercel build issue.

--- packages/auth/src/client.ts
+++ packages/auth/src/client.ts
@@ -1,9 +1,12 @@
 import { createBrowserClient } from '@supabase/ssr'
 import { Database } from '@autocrm/core'
+import { SupabaseClient } from '@supabase/supabase-js'
 
-export const createClient = () => {
+export const createClient = (): SupabaseClient<Database> => {
   return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
-} 
+}
+
+export type Client = ReturnType<typeof createClient> 
